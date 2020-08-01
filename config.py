41125d71355f65bd529edb99434df60a9e7f58d7
Disha Srivastava <disha.srivastava07@gmail.com>
2020-08-01 09:53:53 -0700

    Setting up configuration

---
+++ config.py
@@ -0,0 +1,6 @@
+#Create Crediential Keys
+
+consumer_key = 'V6XKhulVp7YYY3jVJAE5yZHcR'
+consumer_secret = 'k6ydihnwe2Oue1ITswLVTSHrD5YpLQyNIQlWrxlLnvBYwjRFuL'
+access_token = '2620473211-yUybtaw3CDNd9vyHh9qSvgOul1pkiOuB0wpVEts'
+access_token_secret = 'srxKoqEdF0SJbBnB8K2mgcz1m7AOJLUVHofIB5yyycXAZ'
---
+++ tool.py
@@ -0,0 +1,8 @@
+import tweepy
+import config
+
+#Set Up Configuration
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+api = tweepy.API(auth)
+
