e616a1f68b519e66e9d6425770f6317003d8314b
Disha Srivastava <disha.srivastava07@gmail.com>
2020-08-01 11:52:58 -0700

    Geo accessing working

--- Back-End /tool.py
+++ Back-End /tool.py
@@ -1,4 +1,8 @@
+#Import Packages
 import tweepy
+import os
+import tweepy as tw
+import pandas as pd
 
 #Create Crediential Keys
 consumer_key = 'V6XKhulVp7YYY3jVJAE5yZHcR'
@@ -9,10 +13,22 @@ access_token_secret = 'srxKoqEdF0SJbBnB8K2mgcz1m7AOJLUVHofIB5yyycXAZ'
 #Set Up Configuration
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
-#Run Number of Tweets Program
-user = api.get_user('dishsrivastava')
-print(user.screen_name)
-print('Followers Count: ' + str(user.followers_count))
-print('Tweets Count: ' + str(user.statuses_count))
+# Define the search term and the date_since date as variables
+new_search = "presidential+election -filter:retweets" #-filter removes retweets
+date_since = "2020-01-01"
+
+# Collect tweets
+tweets = tw.Cursor(api.search,
+              q=new_search,
+              lang="en",
+              since=date_since).items(5)
+
+users_locs = [[tweet.user.screen_name, tweet.user.location, tweet.text] for tweet in tweets]
+
+#Print tweets and attached attributes
+for tweet in tweets:
+    print(users_locs)
+
+
